4c503ebd671978ab8c0c50f181377eb4df43da93
Abhishek Gowda <Abhishek Gowda@ABHISHEK-K-H-96>
2022-09-21 12:16:11 +0530

    comments added to database

--- src/test/java/org/sdet40/practice/FetchDataFRromDataBase.java
+++ src/test/java/org/sdet40/practice/FetchDataFRromDataBase.java
@@ -10,6 +10,7 @@ public class FetchDataFRromDataBase {
 
 	public static void main(String[] args) throws SQLException {
 		DataBaseUtility databaseUtility = new DataBaseUtility();
+		//start connection
 		databaseUtility.openDBConnection(IConstantUtility.DB_URL, "root", "root");
 		List<String> data = databaseUtility.getDataFromDB("select * from sdet40;", "Emp_Table");
 		System.out.println(data);
